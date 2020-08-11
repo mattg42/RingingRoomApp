0464a63c135956a8374cab5ab02fe9f7711b95af
Matthew Goodship <matthewwgoodship@icloud.com>
2020-08-11 16:04:30 +0100

    Added tower and user objects

---
+++ iOSRingingRoom/Tower.swift
@@ -0,0 +1,17 @@
+//
+//  Tower.swift
+//  iOSRingingRoom
+//
+//  Created by Matthew on 11/08/2020.
+//  Copyright © 2020 Matthew Goodship. All rights reserved.
+//
+
+import Foundation
+
+class Tower {
+    var id:String!
+    var name:String!
+    var hosts:[String]!
+    var permitHostMode:Bool!
+    var creator:String!
+}
---
+++ iOSRingingRoom/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  iOSRingingRoom
+//
+//  Created by Matthew on 11/08/2020.
+//  Copyright © 2020 Matthew Goodship. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    var username:String!
+    var recentTowers:[Tower]!
+    var bookmarkedTowers:[Tower]!
+    var createdTowers:[Tower]!
+    var hostTowers:[Tower]!
+}
