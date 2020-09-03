ceee2047ea9d5f4d9fe66db24b79ac8508ba77e8
Matthew Goodship <matthewwgoodship@icloud.com>
2020-09-03 16:08:59 +0100

    added listeners for server events

--- iOSRingingRoom/SocketIOManager.swift
+++ iOSRingingRoom/SocketIOManager.swift
@@ -14,8 +14,11 @@ class SocketIOManager: NSObject {
     
     var manager:SocketManager!
     
-    init(server_ip:String) {
+    var ringingroomView:RingingRoomView!
+    
+    init(server_ip:String, ringingRoomView:RingingRoomView) {
         super.init()
+        self.ringingroomView = ringingRoomView
         manager = SocketManager(socketURL: URL(string: server_ip)!, config: [.log(false), .compress])
         socket = manager.defaultSocket
         addListeners()
@@ -23,7 +26,9 @@ class SocketIOManager: NSObject {
     
     func addListeners() {
         socket.onAny() { data in
-         //   print("description: ", data.description)
+            if !(data.event == "ping" || data.event == "pong") {
+                print("received socketio event: ", data.event)
+            }
         }
         
         socket.on(clientEvent: .connect) { data, ack in
@@ -32,124 +37,94 @@ class SocketIOManager: NSObject {
     
         socket.on("s_bell_rung") { data, ack in
             print("received bell rung")
-            for line in data {
-                print(line)
-//            print("Received event: " + data["global_bell_state"] + data["who_rang"])
-//            // if(msg.disagree) {}
-//            print("ring bell", data["who_rang"])
-//          //  bell_circle.ring_bell(msg.who_rang)
-            }
+            self.ringingroomView.bellCircle.bells[self.getDict(data)["who_rang"] as! Int].stroke.toggle()
+            self.ringingroomView.bellRang(number: self.getDict(data)["who_rang"] as! Int)
         }
 
-        // Userlist was set
-//        socket.on('s_set_userlist', function(msg,cb){
-//            console.log('s_set_userlist: ' + msg.user_list)
-//            bell_circle.$refs.users.user_names = msg.user_list
-//        })
+        socket.on("s_set_userlist") { data, ack in
+            self.ringingroomView.users = self.getDict(data)["user_list"] as! [String]
+        }
 //
-//        // User entered the room
-//        socket.on('s_user_entered', function(msg, cb){
-//            console.log(msg.user_name + ' entered')
-//            bell_circle.$refs.users.add_user(msg.user_name)
-//        })
+        // User entered the room
+        socket.on("s_user_entered") { data, ack in
+            self.ringingroomView.users.append(self.getDict(data)["user_name"] as! String)
+        }
 //
 //        // User left the room
-//        socket.on('s_user_left', function(msg, cb){
-//            console.log(msg.user_name + ' left')
+//        socket.on("s_user_left", function(msg, cb){
+//            console.log(msg.user_name + " left")
 //            bell_circle.$refs.users.remove_user(msg.user_name)
 //            bell_circle.$refs.bells.forEach((bell,index)=>
 //                {
 //                    if (bell.assigned_user === msg.user_name) {
-//                        bell.assigned_user = ''
+//                        bell.assigned_user = ""
 //                    }
 //                })
 //        })
 //
 //        // Number of observers changed
-//        socket.on('s_set_observers', function(msg, cb){
-//            console.log('observers: ' + msg.observers)
+//        socket.on("s_set_observers", function(msg, cb){
+//            console.log("observers: " + msg.observers)
 //            bell_circle.$refs.users.observers = msg.observers
 //        })
 //
-//        // User was assigned to a bell
-//        socket.on('s_assign_user', function(msg, cb){
-//            console.log('Received user assignment: ' + msg.bell + ' ' + msg.user)
-//            try {
-//                // This stochastically very error-prone:
-//                // Sometimes it sets the state before the bell is created
-//                // As such: try it, but if it doesn't work wait a bit and try again.
-//                bell_circle.$refs.bells[msg.bell - 1].assigned_user = msg.user
-//                if (msg.user === window.tower_parameters.cur_user_name){
-//                    bell_circle.$refs.users.rotate_to_assignment()
-//                }
-//            } catch(err) {
-//                console.log('caught error assign_user trying again')
-//                setTimeout(100, function(){
-//                    bell_circle.$refs.bells[msg.bell - 1].assigned_user = msg.user
-//                    if (msg.user === window.tower_parameters.cur_user_name){
-//                        bell_circle.$refs.users.rotate_to_assignment()
-//                    }
-//                })
-//            }
-//        })
+        // User was assigned to a bell
+        socket.on("s_assign_user") { data, ack in
+            var assignments = self.ringingroomView.bellCircle.assignments
+            assignments[self.getDict(data)["bell"] as! Int - 1] = self.getDict(data)["user"] as! String
+            self.ringingroomView.bellCircle.assignments = assignments
+        }
 //
 //        // A call was made
-//        socket.on('s_call',function(msg,cb){
-//            console.log('Received call: ' + msg.call)
+//        socket.on("s_call",function(msg,cb){
+//            console.log("Received call: " + msg.call)
 //            bell_circle.$refs.display.make_call(msg.call)
 //        })
 //
 //        // The server told us the number of bells in the tower
-//        socket.on('s_size_change', function(msg,cb){
+//        socket.on("s_size_change", function(msg,cb){
 //            var new_size = msg.size
 //            bell_circle.number_of_bells = new_size
 //        })
 //
 //
-//        // The server sent us the global state set all bells accordingly
-//        socket.on('s_global_state',function(msg,cb){
-//            var gstate = msg.global_bell_state
-//            for (var i = 0 i < gstate.length i++){
-//                try {
-//                    // This stochastically very error-prone:
-//                    // Sometimes it sets the state before the bell is created
-//                    // As such: try it, but if it doesn't work wait a bit and try again.
-//                    bell_circle.$refs.bells[i].set_state_silently(gstate[i])
-//                } catch(err) {
-//                    console.log('caught error set_state trying again')
-//                    setTimeout(100, function(){
-//                        bell_circle.$refs.bells[i].set_state_silently(gstate[i])
-//                    })
-//                }
-//            }
-//        })
+        // The server sent us the global state set all bells accordingly
+        socket.on("s_global_state") { data, ack in
+            for (index, state) in (self.getDict(data)["global_bell_state"] as! [Bool]).enumerated() {
+                self.ringingroomView.bellCircle.bells[index].stroke = state ? .handstroke : .backstroke
+            }
+        }
 //
-//        // The server told us whether to use handbells or towerbells
-//        socket.on('s_audio_change',function(msg,cb){
-//          console.log('changing audio to: ' + msg.new_audio)
-//          bell_circle.$refs.controls.audio_type = msg.new_audio
-//          bell_circle.audio = msg.new_audio == 'Tower' ? tower : hand
-//          // Make sure the volume is set consistently
-//          bell_circle.audio._volume = window.user_parameters.bell_volume * 0.1
-//        })
+        // The server told us whether to use handbells or towerbells
+        socket.on("s_audio_change") { data, ack in
+            print("changing audio to: \(self.getDict(data)["new_audio"])")
+            if self.getDict(data)["new_audio"] as! String == "Tower" {
+                self.ringingroomView.bellCircle.bellType = .tower
+            } else {
+                self.ringingroomView.bellCircle.bellType = .hand
+            }
+        }
 //
 //        // A chat message was received
-//        socket.on('s_msg_sent', function(msg,cb){
+//        socket.on("s_msg_sent", function(msg,cb){
 //            bell_circle.$refs.chatbox.messages.push(msg)
-//            if(msg.email != window.tower_parameters.cur_user_email && !$('#chat_input_box').is(':focus')) {
+//            if(msg.email != window.tower_parameters.cur_user_email && !$("#chat_input_box").is(":focus")) {
 //                bell_circle.unread_messages++
 //            }
 //            bell_circle.$nextTick(function()
-//                $('#chat_messages').scrollTop($('#chat_messages')[0].scrollHeight)
+//                $("#chat_messages").scrollTop($("#chat_messages")[0].scrollHeight)
 //            })
 //        })
 //
-//        // Host mode was changed
-//        socket.on('s_host_mode', function(msg,cb){
-//            bell_circle.$refs.controls.host_mode = msg.new_mode
-//        })
+        // Host mode was changed
+        socket.on("s_host_mode") { data, ack in
+            self.ringingroomView.hostModeEnabled = self.getDict(data)["new_mode"] as! Bool
+        }
     }
     
+    func getDict(_ data:[Any]) -> [String:Any] {
+        return (data[0] as! [String:Any])
+    }
     
     func connectSocket() {
         socket.connect()
