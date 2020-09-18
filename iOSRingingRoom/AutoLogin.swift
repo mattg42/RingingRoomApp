24e1d0ce093981f2289be026d09b1633856379a4
Matthew Goodship <matthewwgoodship@icloud.com>
2020-09-18 18:20:48 +0100

    Made autologin view look like launchscreen

--- iOSRingingRoom/AutoLogin.swift
+++ iOSRingingRoom/AutoLogin.swift
@@ -9,9 +9,30 @@
 import SwiftUI
 
 struct AutoLogin: View {
+    @State var showingAlert = false
     var body: some View {
+        ZStack {
+            Color.main
+            .edgesIgnoringSafeArea(.all)
+            HStack {
+                Spacer(minLength: 55)
+                Image("rrLogo").resizable()
+                    .aspectRatio(contentMode: .fit)
+                Spacer(minLength: 55)
+            }
+            .padding(.top, -20)
+        }
+        .alert(isPresented: $showingAlert) {
+        //present welcome login screen
+            Alert(title: Text("Error"), message: Text("There was an error trying to log you in."), dismissButton: .cancel(Text("OK"), action: {
+                self.viewControllerHolder?.present(style: .fullScreen, name: "", animated: false) {
+                    WelcomeLoginScreen()
+                }
+            }))
+        }
     }
 }
+
 struct AutoLogin_Previews: PreviewProvider {
     static var previews: some View {
         AutoLogin()
