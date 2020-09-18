fd6583e292eecc95576cd302ed155b48fa40259b
Matthew Goodship <matthewwgoodship@icloud.com>
2020-09-18 18:19:33 +0100

    Create AutoLogin.swift

---
+++ iOSRingingRoom/AutoLogin.swift
@@ -0,0 +1,19 @@
+//
+//  AutoLogin.swift
+//  iOSRingingRoom
+//
+//  Created by Matthew on 05/09/2020.
+//  Copyright © 2020 Matthew Goodship. All rights reserved.
+//
+
+import SwiftUI
+
+struct AutoLogin: View {
+    var body: some View {
+    }
+}
+struct AutoLogin_Previews: PreviewProvider {
+    static var previews: some View {
+        AutoLogin()
+    }
+}
