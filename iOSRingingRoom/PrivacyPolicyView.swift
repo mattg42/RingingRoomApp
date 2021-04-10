3f0682c94864b1a75090f902a3529f757f1365f1
Matthew Goodship <matthewwgoodship@icloud.com>
2021-04-10 12:22:59 +0100

    Updated privacy policy

--- iOSRingingRoom/PrivacyPolicyView.swift
+++ iOSRingingRoom/PrivacyPolicyView.swift
@@ -49,7 +49,7 @@ struct PrivacyPolicyView: View {
     
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
-            Text("The Ringing Room app is independently operated by Matthew Goodship. This app doesn't collect any personal information. However, it does use the Ringing Room API to function. Therefore Ringing Room is able to see all the requests made and use that information, such as your email address, as they see fit. By using this app, you are subject to Ringing Room's Privacy Policy, which can be found at")
+            Text("The Ringing Room app is independently operated by Matthew Goodship. This app doesn’t collect any personal information, but it does collect crash reports and performance data if you have opted in on your device (you can find the setting by going to Settings > Privacy > Analytics and Improvements > Share With App Developers). However, it does use the Ringing Room API to function. Therefore Ringing Room is able to see all the requests made and use that information, such as your email address, as they see fit. By using this app, you are subject to Ringing Room’s Privacy Policy, which can be found at")
             Button("ringingroom.com/privacy") {isShowingPrivacyPolicy = true}
         }
         .sheet(isPresented: $isShowingPrivacyPolicy, content: {
